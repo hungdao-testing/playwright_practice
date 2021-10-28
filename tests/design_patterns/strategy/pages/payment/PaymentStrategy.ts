3bd33a40c7b6359128a5429801cee7c51726d36b
hungdao-testing <daoquanghunggtvt@gmail.com>
2021-10-28 20:52:54 +0700

    add reference link

--- tests/design_patterns/strategy/pages/payment/PaymentStrategy.ts
+++ tests/design_patterns/strategy/pages/payment/PaymentStrategy.ts
@@ -1,6 +1,7 @@
 export type ICreditCard = { "cc": string, "year": string, "cvv": string }
 export type INetBanking = { "bank": string, "account": string, "pin": string }
 
+// Ref: https://stackoverflow.com/questions/53392498/avoid-typescript-casting-inside-a-switch
 export type TPaymentData = {
     CREDIT_CARD: ICreditCard,
     NET_BANKING: INetBanking

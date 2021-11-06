71b9ab9b09055d84361c60ba38b8f3f20a0e06a9
hungdao-testing <daoquanghunggtvt@gmail.com>
2021-11-06 22:52:24 +0700

    rename

--- tests/design_patterns/strategy/pages/payment/PaymentStrategy.ts
+++ tests/design_patterns/strategy/pages/payment/PaymentStrategy.ts
@@ -2,15 +2,15 @@ export type CreditCardObj = { "cc": string, "year": string, "cvv": string }
 export type NetBankingObj = { "bank": string, "account": string, "pin": string }
 
 // Ref: https://stackoverflow.com/questions/53392498/avoid-typescript-casting-inside-a-switch
-export type PaymentObjectType = {
+export type PaymentTypeMap = {
     CREDIT_CARD: CreditCardObj,
     NET_BANKING: NetBankingObj
 }
 
-export type PaymentType = keyof PaymentObjectType;
+export type PaymentType = keyof PaymentTypeMap;
 export type PaymentObject<T extends PaymentType> = T extends any ? {
     type: T;
-    data: PaymentObjectType[T];
+    data: PaymentTypeMap[T];
 } : never;
 
 

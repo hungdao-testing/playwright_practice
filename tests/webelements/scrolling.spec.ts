9066fc18064722b937a05a2b46ee6234be62b585
Hung Dao <51052135+hungdao-testing@users.noreply.github.com>
2022-07-22 18:09:47 +0700

    Update scrolling.spec.ts

--- tests/webelements/scrolling.spec.ts
+++ tests/webelements/scrolling.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Locator, chromium } from '@playwright/test';
 
 async function scrollToInsideLocator(toText: string, outsideLocator: Locator, insideLocator: string) {
-  let numRecord = await outsideLocator.locator(insideLocator).count();
+  let numRecord = parseInt((await outsideLocator.locator(insideLocator).locator('td').first().innerText()).trim());
   let innerLocator = outsideLocator.locator(insideLocator);
 
   for (let i = 1; i <= numRecord; i++) {
@@ -10,11 +10,14 @@ async function scrollToInsideLocator(toText: string, outsideLocator: Locator, in
 
     let desiredLocator = innerLocator.filter({ hasText: toText });
     let isDesiredLocatorVisible = await desiredLocator.isVisible();
-    if (isDesiredLocatorVisible) return desiredLocator;
+    if (isDesiredLocatorVisible) {
+      await desiredLocator.hover()
+      return desiredLocator
+    };
 
     await outsideLocator.locator(insideLocator).first().scrollIntoViewIfNeeded();
     await outsideLocator.locator(insideLocator).last().hover();
-    numRecord = await outsideLocator.locator(insideLocator).count();
+    numRecord = parseInt((await outsideLocator.locator(insideLocator).locator('td').first().innerText()).trim())
 
     console.log("Update number of record:: ", numRecord)
   }
@@ -24,8 +27,8 @@ async function scrollToInsideLocator2(toText: string, outsideLocator: Locator, i
   let innerLocator = outsideLocator.locator(insideLocator);
   let desiredLocator = innerLocator.filter({ hasText: toText });
   let isDesiredLocatorVisible = await desiredLocator.isVisible();
-  
-  while(!isDesiredLocatorVisible){
+
+  while (!isDesiredLocatorVisible) {
     desiredLocator = innerLocator.filter({ hasText: toText });
     isDesiredLocatorVisible = await desiredLocator.isVisible();
     await outsideLocator.locator(insideLocator).first().scrollIntoViewIfNeeded();
@@ -36,8 +39,8 @@ async function scrollToInsideLocator2(toText: string, outsideLocator: Locator, i
 }
 
 test.describe.parallel('Virtual loading table', () => {
-  test('Chrome - Scroll to find a record and get content', async ({}) => {
-    const browser = await chromium.launch({ slowMo: 200 });
+  test('Chrome - Scroll to find a record and get content', async ({ }) => {
+    const browser = await chromium.launch();
     const browserContext = await browser.newContext();
     const page = await browserContext.newPage();
     await page.goto('http://primefaces.org/primeng/table/virtualscroll');
@@ -45,11 +48,12 @@ test.describe.parallel('Virtual loading table', () => {
 
     await tableLocator.waitFor({ state: 'visible' });
     // await page.pause()
-    const innerLocator = await scrollToInsideLocator2('70', tableLocator, 'tr');
+    const innerLocator = await scrollToInsideLocator('222', tableLocator, 'tr');
 
     if (!innerLocator) throw Error('No Locator found to grab text');
     const text = await innerLocator.locator('td').last().innerText();
+    console.log("TEXT:: ", text)
     expect(text.length).toBeGreaterThan(1);
-    await page.close();
+    await page.pause();
   });
 });

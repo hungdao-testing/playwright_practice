1cc3e2e13164d1d9b462042da1d2f943ddf13262
hungdao-testing <daoquanghunggtvt@gmail.com>
2022-07-25 17:22:44 +0700

    enhance scrolling

--- tests/webelements/scrolling.spec.ts
+++ tests/webelements/scrolling.spec.ts
@@ -1,59 +1,55 @@
-import { test, expect, Locator, chromium } from '@playwright/test';
+import { test, expect, Locator, chromium, firefox } from '@playwright/test';
 
-async function scrollToInsideLocator(toText: string, outsideLocator: Locator, insideLocator: string) {
-  let numRecord = parseInt((await outsideLocator.locator(insideLocator).locator('td').first().innerText()).trim());
-  let innerLocator = outsideLocator.locator(insideLocator);
-
-  for (let i = 1; i <= numRecord; i++) {
-    console.log("Init number of record:: ", numRecord)
-    console.log("Init i:: ", i)
-
-    let desiredLocator = innerLocator.filter({ hasText: toText });
-    let isDesiredLocatorVisible = await desiredLocator.isVisible();
-    if (isDesiredLocatorVisible) {
-      await desiredLocator.hover()
-      return desiredLocator
-    };
-
-    await outsideLocator.locator(insideLocator).first().scrollIntoViewIfNeeded();
-    await outsideLocator.locator(insideLocator).last().hover();
-    numRecord = parseInt((await outsideLocator.locator(insideLocator).locator('td').first().innerText()).trim())
-
-    console.log("Update number of record:: ", numRecord)
+async function scrollToRowId(rowId: string, tableLocator: Locator, selectorOfRowId: string) {
+  await tableLocator.locator('tbody').hover();
+  let isScroll = true;
+  while (isScroll) {
+    await tableLocator.locator('tbody').page().waitForSelector('.p-scroller-loading', {state: 'hidden'})
+    let queryLocator = tableLocator.locator(`*css=${selectorOfRowId}`).filter({ hasText: rowId });
+    let isVisible = await queryLocator.isVisible();
+    if (isVisible) {
+      isScroll = false;
+      await queryLocator.hover()
+      return queryLocator;
+    }
+    await tableLocator.locator('tbody').page().mouse.wheel(0, 10);
+    
   }
 }
 
-async function scrollToInsideLocator2(toText: string, outsideLocator: Locator, insideLocator: string) {
-  let innerLocator = outsideLocator.locator(insideLocator);
-  let desiredLocator = innerLocator.filter({ hasText: toText });
-  let isDesiredLocatorVisible = await desiredLocator.isVisible();
-
-  while (!isDesiredLocatorVisible) {
-    desiredLocator = innerLocator.filter({ hasText: toText });
-    isDesiredLocatorVisible = await desiredLocator.isVisible();
-    await outsideLocator.locator(insideLocator).first().scrollIntoViewIfNeeded();
-    await outsideLocator.locator(insideLocator).last().hover();
-  }
-
-  return desiredLocator;
-}
-
-test.describe.parallel('Virtual loading table', () => {
-  test('Chrome - Scroll to find a record and get content', async ({ }) => {
-    const browser = await chromium.launch();
+test.describe.parallel('Virtual scrolling', () => {
+  test('with Preloaded Data - Scroll to find a record and get content', async ({}, testInfo) => {
+    const browser = await chromium.launch({headless: true});
     const browserContext = await browser.newContext();
     const page = await browserContext.newPage();
     await page.goto('http://primefaces.org/primeng/table/virtualscroll');
     const tableLocator = page.locator('#pr_id_2-table');
 
     await tableLocator.waitFor({ state: 'visible' });
-    // await page.pause()
-    const innerLocator = await scrollToInsideLocator('222', tableLocator, 'tr');
-
+    const innerLocator = await scrollToRowId('25', tableLocator, 'tr >> td:nth-child(1)');
     if (!innerLocator) throw Error('No Locator found to grab text');
-    const text = await innerLocator.locator('td').last().innerText();
-    console.log("TEXT:: ", text)
+    await innerLocator.screenshot({path: 'preloaded.png'})
+    
+    const text = await innerLocator.innerText();
+    
+    console.log("Preloaded Data::", text)
     expect(text.length).toBeGreaterThan(1);
-    await page.pause();
+
+  });
+  test('with Lazy Loading from a Remote Datasource - Scroll to find a record and get content', async ({}) => {
+    const browser = await firefox.launch({headless: true});
+    const browserContext = await browser.newContext();
+    const page = await browserContext.newPage();
+    await page.goto('http://primefaces.org/primeng/table/virtualscroll');
+    const tableLocator = page.locator('#pr_id_3-table');
+
+    await tableLocator.waitFor({ state: 'visible' });
+    const innerLocator = await scrollToRowId('25', tableLocator, 'tr >> td:nth-child(1)');
+    if (!innerLocator) throw Error('No Locator found to grab text');
+    await innerLocator.screenshot({path: 'lazyloaded.png'})
+    const text = await innerLocator.innerText();
+    console.log("Lazy Loading::", text)
+    expect(text.length).toBeGreaterThan(1);
+
   });
 });

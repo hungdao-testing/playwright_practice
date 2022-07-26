19e3e1737dd429b999c2bb04a334ae8280e56141
hungdao-testing <daoquanghunggtvt@gmail.com>
2022-07-26 12:03:11 +0700

    handling null for frame

--- tests/webelements/upload.spec.ts
+++ tests/webelements/upload.spec.ts
@@ -8,10 +8,10 @@ test("Upload", async ({ page }) => {
     await page.waitForSelector("#iframecontainer")
 
     const frameEle = await page.$("div#iframewrapper iframe");
-    const frameContent = await frameEle.contentFrame();
-    expect(await frameContent.innerText('h3')).toEqual("A demonstration of how to access a File Upload Button")
+    const frameContent = await frameEle!.contentFrame();
+    expect(await frameContent!.innerText('h3')).toEqual("A demonstration of how to access a File Upload Button")
     // await frameContent.waitForSelector("input#myFile")
-    await frameContent.setInputFiles("input#myFile", './upload_files/image1.jpg');
+    await frameContent!.setInputFiles("input#myFile", './upload_files/image1.jpg');
 })
 
 test("Upload multiple file @debug", async({page}) => {
